e7eddf418ada6d9c0efe2fca39de15738918753c
SiddharthJBoiMenon <112028129+SiddharthJBoiMenon@users.noreply.github.com>
2022-08-24 17:34:45 -0400

    Add files via upload

---
+++ IGME 206/Menon_HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menon_HelloWorld
+{
+    internal class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            int playingAroundVar;
+            Console.WriteLine("Hello World!"); // This statement allows the computer to greet the entire world quite politely.
+            playingAroundVar = false != true ? 1 : 1; // Absolutely redundant I'm not sure what to do without an objective.
+            if (playingAroundVar == 1)
+                Console.WriteLine("This *if statement* condition turned out to be true");
+            else
+                Console.WriteLine("If this statement prints humanity will be gifted the truth about the universe and the meaning of life.");
+
+            //I apologize if this content was inappropriate I promise that future assignments shall not be this immature.
+        }
+    }
+}
